eddf29017ea871ebcb950f20fd6e35fb5ee99332
nathaaliie123 <nathalie.djuranovik@gmail.com>
2024-12-08 11:35:52 +0700

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+#Menampilkan judul
+st.title("Welcome to My First Streamlit App")
+
+# Menampilkan tulisan
+st.write("Hello Everyone, this is the first python application")
